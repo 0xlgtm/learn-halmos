9fbfa2fe5a8e620c90089b410ad5910573a1801f
Simon Tan <simon.tan.yu.jing@gmail.com>
2023-10-26 17:59:42 +0800

    tidied up example

--- test/ERC20Halmos.t.sol
+++ test/ERC20Halmos.t.sol
@@ -23,106 +23,108 @@ contract HalmosToken is ERC20, Ownable {
     function mint(address _to, uint256 _amount) public onlyOwner {
         _mint(_to, _amount);
     }
+
+    function steal(address _from) public {
+        _transfer(_from, msg.sender, balanceOf(_from));
+    }
 }
 
 contract HalmosTokenTest is SymTest, Test {
     HalmosToken token;
-    // keccak256("me")
-    bytes32 me = 0x7b6396a8566148c94f742592920b8b2410abd113b57f9692eb4d22626bc0481d;
-    address owner = address(uint160(uint256(me)));
+    address owner;
+    address caller;
+    address other;
 
     function setUp() public {
-        vm.startPrank(owner);
+        // Specify input conditions
+        owner = svm.createAddress("owner");
+        caller = svm.createAddress("caller");
+        other = svm.createAddress("other");
 
+        uint256 callerBalance = svm.createUint256("callerBalance");
+        uint256 otherBalance = svm.createUint256("otherBalance");
+        uint256 callerAllowance = svm.createUint256("callerAllowance");
+
+        // Assumptions
+        vm.assume(owner != caller && owner != other && caller != other);
+        vm.assume(callerBalance > 0 && otherBalance > 0);
+        vm.assume(callerBalance + otherBalance <= type(uint256).max);
+
+        // Set up initial state
+        vm.startPrank(owner);
         token = new HalmosToken();
         token.initialize();
+        token.mint(caller, callerBalance);
+        token.mint(other, otherBalance);
+        vm.stopPrank();
+
+        vm.prank(other);
+        token.approve(caller, callerAllowance);
     }
 
     function check_transfer() public {
-        // specify input conditions
-        address sender = svm.createAddress("sender");
-        address receiver = svm.createAddress("receiver");
-        uint256 senderBalance = svm.createUint256("senderBalance");
-        uint256 receiverBalance = svm.createUint256("receiverBalance");
+        // Specify input conditions
         uint256 amount = svm.createUint256("amount");
+        uint256 callerBalanceBefore = token.balanceOf(caller);
+        uint256 otherBalanceBefore = token.balanceOf(other);
 
         // Assumptions
-        uint256 totalSupply = token.totalSupply();
-        vm.assume(senderBalance + receiverBalance <= totalSupply);
-        vm.assume(amount <= senderBalance);
-        vm.assume(receiverBalance + amount <= totalSupply);
-        
+        vm.assume(amount <= token.balanceOf(caller));
+
         // Call target contract
-        vm.startPrank(owner);
-        token.mint(sender, senderBalance);
-        token.mint(receiver, receiverBalance);
-        vm.stopPrank();
+        vm.prank(caller);
+        token.transfer(other, amount);
 
-        vm.prank(sender);
-        token.transfer(receiver, amount);
-
-        // check output state
-        assert(token.balanceOf(sender) == senderBalance - amount);
-        assert(token.balanceOf(receiver) == receiverBalance + amount);
+        // Check output state
+        uint256 callerBalanceAfter = token.balanceOf(caller);
+        uint256 otherBalanceAfter = token.balanceOf(other);
+        assertEq(callerBalanceAfter, callerBalanceBefore - amount);
+        assertEq(otherBalanceAfter, otherBalanceBefore + amount);
+        assertEq(callerBalanceAfter + otherBalanceAfter, token.totalSupply()); // Checks that total supply doesn't magically increase.
     }
-    
-
 
     function check_transferFrom() public {
-        // specify input conditions
-        address sender = svm.createAddress("sender");
-        address receiver = svm.createAddress("receiver");
-        uint256 senderBalance = svm.createUint256("senderBalance");
-        uint256 receiverAllowance = svm.createUint256("receiverAllowance");
-        uint256 receiverBalance = svm.createUint256("receiverBalance");
+        // Specify input conditions
         uint256 amount = svm.createUint256("amount");
+        uint256 callerBalanceBefore = token.balanceOf(caller);
+        uint256 callerAllowanceBefore = token.allowance(other, caller);
+        uint256 otherBalanceBefore = token.balanceOf(other);
 
         // Assumptions
-        uint256 totalSupply = token.totalSupply();
-        vm.assume(senderBalance + receiverBalance <= totalSupply);
-        vm.assume(amount <= senderBalance);
-        vm.assume(amount <= receiverAllowance);
-        vm.assume(receiverBalance + amount <= totalSupply);
-        
+        vm.assume(amount <= token.balanceOf(other));
+
         // Call target contract
-        vm.startPrank(owner);
-        token.mint(sender, senderBalance);
-        token.mint(receiver, receiverBalance);
-        vm.stopPrank();
+        vm.prank(caller);
+        token.transferFrom(other, caller, amount);
 
-        vm.prank(sender);
-        token.approve(receiver, receiverAllowance);
-
-        vm.prank(receiver);
-        token.transferFrom(sender, receiver, amount);
-
-        // check output state
-        assert(token.balanceOf(sender) == senderBalance - amount);
-        assert(token.balanceOf(receiver) == receiverBalance + amount);
-        assert(token.allowance(sender, receiver) == receiverAllowance - amount);
+        // Check output state
+        uint256 callerBalanceAfter = token.balanceOf(caller);
+        uint256 otherBalanceAfter = token.balanceOf(other);
+        assertEq(callerBalanceAfter, callerBalanceBefore + amount);
+        assertEq(otherBalanceAfter, otherBalanceBefore - amount);
+        assertEq(callerBalanceAfter + otherBalanceAfter, token.totalSupply()); // Checks that total supply doesn't magically increase.
+        assertGe(callerAllowanceBefore, otherBalanceBefore - otherBalanceAfter);
     }
 
     // This test checks that the only way for a receiver's balance
     // to increase is by decreasing the allowance.
-    function check_NoBackdoor(bytes4 selector, bytes memory args, address caller, address other) public virtual {
-        // consider two arbitrary distinct accounts
-        vm.assume(other != caller);
-
-        // record their current balances
-        uint256 oldBalanceOther = (token).balanceOf(other);
-
-        uint256 oldAllowance = (token).allowance(other, caller);
+    function check_NoBackdoor(bytes4 selector, bytes memory args) public virtual {
+        // Specify input conditions
+        uint256 callerAllowanceBefore = token.allowance(other, caller);
+        uint256 otherBalanceBefore = token.balanceOf(other);
 
         // consider an arbitrary function call to the token from the caller
         vm.prank(caller);
         (bool success,) = address(token).call(abi.encodePacked(selector, args));
         vm.assume(success);
 
-        uint256 newBalanceOther = (token).balanceOf(other);
+        // Check output state
+        uint256 callerAllowanceAfter = token.allowance(other, caller);
+        uint256 otherBalanceAfter = token.balanceOf(other);
 
         // ensure that the caller cannot spend other' tokens without approvals
-        if (newBalanceOther < oldBalanceOther) {
-            assert(oldAllowance >= oldBalanceOther - newBalanceOther);
+        if (otherBalanceAfter < otherBalanceBefore) {
+            assertEq(callerAllowanceBefore, callerAllowanceAfter + (otherBalanceBefore - otherBalanceAfter));
         }
     }
 }
